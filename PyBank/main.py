386bec26738029b99060782645c6dc53dcce4283
Jamie Diaz <jamie.diaz@gmail.com>
2019-06-22 22:36:06 -0500

    created a list to store all values from CSV.  Looking for a way to get a difference between items in the list.

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,7 @@ with open(csvpath, newline="") as csvfile:
     csv_header = next(csvfile)
     
     total_months = sum(1 for row in csvreader)
-    print (f" There are {total_months} months.")
+    print (f"There are {total_months} months.")
     
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -17,11 +17,33 @@ with open(csvpath, newline="") as csvfile:
     csv_header = next(csvfile)
 
     net_profit = 0
-    for row2 in csvreader:
+    for row in csvreader:
         
-        net_profit += int(row2[1])
+        net_profit += int(row[1])
+        net_average = net_profit / total_months
         
-    print (net_profit)
+    print (f"The netprofit/loss is {net_profit}.")
+    print (net_average)
 
+
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvfile)
+
+    
+    row_val = []
+    for row in csvreader:
+        row_val.append(int(row[1]))
+        
+print (row_val)    
+change_val = []
+for i in row_val:
+    delta = 0
+    delta = int(row_val[i+1]) - int(row_val[i])
+    change_val.append(delta)
+
+print (change_val)
+        
 
 
