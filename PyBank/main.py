eea31b1b0fe348060776be4fd2bc25fac78990a5
Jamie Diaz <jamie.diaz@gmail.com>
2019-06-22 20:50:02 -0500

    added the with statements to calculate number of months and net profit calculations.

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,5 +6,22 @@ csvpath = os.path.join("budget_data.csv")
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    csv_header = next(csvfile)
     
+    total_months = sum(1 for row in csvreader)
+    print (f" There are {total_months} months.")
+    
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvfile)
+
+    net_profit = 0
+    for row2 in csvreader:
+        
+        net_profit += int(row2[1])
+        
+    print (net_profit)
+
+
 
