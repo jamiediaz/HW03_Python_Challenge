5e00d3b3a5f32d428348ba9aeac20597476e2248
Jamie Diaz <jamie.diaz@gmail.com>
2019-06-23 23:08:07 -0500

    how do you calculate percentage in a dict?

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,14 @@
 import os
 import csv
 
+
 csvpath = os.path.join("election_data.csv")
 
 total_votes = 0
 
 
 candidates = []
-
+votes = {}
 
 
 with open(csvpath, newline="") as csvfile:
@@ -19,11 +20,18 @@ with open(csvpath, newline="") as csvfile:
         #calculate total number of votes
         total_votes += 1
 
+
+
         #get a list of candidates
         if row[2] not in candidates:
             candidates.append(row[2])
+            votes[row[2]] = 1
+            # print (votes)
 
-
+        else:
+            votes[row[2]] += 1
+            #print (votes)
+    
 
     
     
@@ -31,3 +39,4 @@ with open(csvpath, newline="") as csvfile:
 
 print (total_votes)    
 print (candidates)
+print (votes)
