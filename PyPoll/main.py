c52c02d8c508e5cf4bfa7ed2ab1fbaecc264ad1f
Jamie Diaz <jamie.diaz@gmail.com>
2019-06-23 22:18:36 -0500

    get a list of candidates

---
+++ PyPoll/main.py
@@ -0,0 +1,33 @@
+import os
+import csv
+
+csvpath = os.path.join("election_data.csv")
+
+total_votes = 0
+
+
+candidates = []
+
+
+
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvfile)
+
+    for row in csvreader:
+        #calculate total number of votes
+        total_votes += 1
+
+        #get a list of candidates
+        if row[1] not in candidates:
+            candidates.append(row[1])
+
+
+
+    
+    
+    
+
+print (total_votes)    
+print (candidates)
