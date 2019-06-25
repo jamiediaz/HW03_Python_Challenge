56d61260a278e33a57b3705abd1e323caa4709bd
Jamie Diaz <jamie.diaz@gmail.com>
2019-06-24 21:51:18 -0500

    final version - output to results.txt

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -37,17 +37,30 @@ with open(csvpath, newline="") as csvfile:
        
     
     
-
+#Print out a total of the votes casted
 print (f"The total votes casted are: {total_votes}.")    
+#Print out a list of the candidates
 print (f"The following candidates are running:\n{candidates}")
+#Print out the number of the votes they got
 print (f"\n-------------Vote Tally----------------\n{votes}")
+
+# It would not allow me to divide by total_votes.  It didn't like mixing int and dictionary values.  So I summed all the votes instead and used that to divide with. 
 sum_votes = sum(votes.values())
 for i in votes:
     votes[i] = float(round((votes[i]/sum_votes)*100))
 
+#Print out what each candidate got in percent
 print (f"\n------Results in percent-----------------\n{votes}")
 
+#Declare a winner by taking the max value and somehow tying in the key associated with that max value. 
 max_val = max(votes.values())
 max_key = [k for k, v in votes.items() if v == max_val]
 
 print (f"\n------------------------------------------\nThe winner is: {max_key} with {max_val}% of the vote")
+
+
+file = open("results.txt","w")
+file.write(f"---------Total Votes-------------\n{total_votes}\n\n")
+file.write(f"---------The results are---------\n{votes}\n\n")
+file.write(f"---------The Winner--------------\n{max_key} with {max_val}%")
+file.close()
