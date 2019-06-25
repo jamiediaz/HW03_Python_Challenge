06a94c0eeeffab41531579d82793f897729624b0
Jamie Diaz <jamie.diaz@gmail.com>
2019-06-25 18:19:58 -0500

    got pyparagraph to at least give some sort of list.

--- ExtraContent/PyParagraph/main.py
+++ ExtraContent/PyParagraph/main.py
@@ -1,3 +1,19 @@
 import re
 import os
+import csv
 
+
+parag = os.path.join("raw_data","paragraph_2.txt")
+with open(parag, newline =None) as textfile:
+    csvreader = csv.reader(textfile, delimiter=" ")
+
+    words = []
+# file = open("raw_data/paragraph_2.txt","r")
+    for column in csvreader:
+        words = column
+        
+        print (words)
+#print (len(words))
+
+
+#file.close()
